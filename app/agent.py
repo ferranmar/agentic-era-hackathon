051f7df09e8a964416f776b9f1f0a0f9c332ea16
Sergio Mota <s.mota@altostratus.es>
2025-03-19 18:35:48 +0100

    last update

--- app/agent.py
+++ app/agent.py
@@ -22,11 +22,13 @@ from google.genai.types import (
     FunctionDeclaration,
     LiveConnectConfig,
     Tool,
+    Schema
 )
 from langchain_google_vertexai import VertexAIEmbeddings
 
 from app.templates import FORMAT_DOCS, SYSTEM_INSTRUCTION
 from app.vector_store import get_vector_store
+from app.interview_agent import InterviewAgent
 
 # Constants
 VERTEXAI = os.getenv("VERTEXAI", "true").lower() == "true"
@@ -53,6 +55,8 @@ embedding = VertexAIEmbeddings(model_name=EMBEDDING_MODEL)
 vector_store = get_vector_store(embedding=embedding, urls=URLS)
 retriever = vector_store.as_retriever()
 
+# Crear una instancia global del agente entrevistador
+interview_agent = InterviewAgent()
 
 def retrieve_docs(query: str) -> dict[str, str]:
     """
@@ -78,7 +82,7 @@ retrieve_docs_tool = Tool(
 )
 
 
-def developer_interview(query: str) -> dict[str, str]:
+def developer_interview_python(anwser: str) -> dict[str, str]:
     """
     Asistente .
 
@@ -89,13 +93,124 @@ def developer_interview(query: str) -> dict[str, str]:
         response to the user
     """
     print("SE LLAMA !!!!!!!!!!!!!!!!!!")
-    return {"output": "¿que es el callback hell?"}
+    print(anwser)
+    return {"question": "¿Cual es tu experiencia en FastAPI?, ¿Cual es tu experiencia en SQLAlchemy?"}
 
 
 # Add the developer interview tool
+developer_interview_tool_python = Tool(
+    function_declarations=[
+        FunctionDeclaration.from_callable(client=genai_client, callable=developer_interview_python)
+    ]
+)
+
+def developer_interview_company(anwser: str) -> dict[str, str]:
+    """
+    Asistente .
+
+    Args:
+        anwser: user answer.
+
+    Returns:
+        response to the user
+    """
+    print("SE LLAMA !!!!!!!!!!!!!!!!!!")
+    print(anwser)
+    return {"question": "El horario de trabajo es de 9 a 18, con un horario de almuerzo de 1 hora. El salario es de 40.000€ brutos anuales. Hay tickets restaurante y de transporte."}
+
+nervous_data = []
+
+def developer_interview_nervous(anwser: str) -> dict[str, str]:
+    """
+    Asistente .
+
+    Args:
+        anwser: user answer.
+
+    Returns:
+        response to the user
+    """
+    print("SE LLAMA !!!!!!!!!!!!!!!!!!")
+    print(anwser)   
+    nervous_data.append(anwser)
+    print(nervous_data)
+    return {"question": "OK"}
+
+def developer_interview(anwser: str) -> dict[str, str]:
+    """
+    Asistente que maneja la entrevista de desarrollo.
+
+    Args:
+        anwser: Respuesta del usuario.
+
+    Returns:
+        Siguiente pregunta o informe final de la entrevista.
+    """
+
+
+    response = interview_agent.process_response(anwser)
+    print(f"response: {response}")
+    return response
+
+
+# Add the developer interview tool
+# developer_interview_tool = Tool(
+#     function_declarations=[
+#         FunctionDeclaration.from_callable(client=genai_client, callable=developer_interview_company),
+#         FunctionDeclaration.from_callable(client=genai_client, callable=developer_interview_python)
+#     ]
+# )
+
 developer_interview_tool = Tool(
     function_declarations=[
-        FunctionDeclaration.from_callable(client=genai_client, callable=developer_interview)
+        # FunctionDeclaration(
+        #     name="developer_interview",
+        #     description="Herramienta para obtener la siguiente pregunta de la entrevista. Tienes que indicar siempre que es lo que ha dicho el usuario",
+        #     parameters=Schema(
+        #         type="object",
+        #         properties={"anwser": {"type": "string", "description": "Respuesta del usuario"}}
+        #     ),
+        #     response=Schema(
+        #         type="object",
+        #         properties={"question": {"type": "string", "description": "Pregunta para la entrevista"}}
+        #     )
+        # ), 
+        FunctionDeclaration(
+            name="developer_interview_nervous",
+            description="Herramienta para obtener la siguiente pregunta de la entrevista. Tienes que indicar siempre que es lo que ha dicho el usuario",
+            parameters=Schema(
+                type="object",
+                properties={"anwser": {"type": "string", "description": "Respuesta del usuario"}}
+            ),
+            response=Schema(
+                type="object",
+                properties={"question": {"type": "string", "description": "Pregunta para la entrevista"}}
+            )
+        ), 
+        FunctionDeclaration(
+            name="developer_interview_company",
+            description="Herramienta para obtener la siguiente pregunta de la entrevista. Tienes que indicar siempre que es lo que ha dicho el usuario",
+            parameters=Schema(
+                type="object",
+                properties={"anwser": {"type": "string", "description": "Respuesta del usuario"}}
+            ),
+            response=Schema(
+                type="object",
+                properties={"question": {"type": "string", "description": "Pregunta para la entrevista"}}
+            )
+        ),
+        FunctionDeclaration(
+            name="developer_interview_python",
+            description="Herramienta para obtener la siguiente pregunta de la entrevista. Tienes que indicar siempre que es lo que ha dicho el usuario",
+            parameters=Schema(
+                type="object",
+                properties={"anwser": {"type": "string", "description": "Respuesta del usuario"}}
+            ),
+            response=Schema(
+                type="object",
+                properties={"question": {"type": "string", "description": "Pregunta para la entrevista"}}
+            )
+        )
     ]
 )
 
@@ -119,12 +234,15 @@ def retrieve_docs(query: str) -> dict[str, str]:
 
 
 tool_functions = {
-    "retrieve_docs": retrieve_docs,
-    "developer_interview": developer_interview,
+    # "retrieve_docs": retrieve_docs,
+    "developer_interview_python": developer_interview_python,
+    "developer_interview_company": developer_interview_company,
+    "developer_interview_nervous": developer_interview_nervous,
+    # "developer_interview": developer_interview
 }
 
 live_connect_config = LiveConnectConfig(
     response_modalities=["AUDIO"],
-    tools=[ developer_interview_tool],
+    tools=[developer_interview_tool],
     system_instruction=Content(parts=[{"text": SYSTEM_INSTRUCTION}]),
 )
--- app/templates.py
+++ app/templates.py
@@ -25,6 +25,43 @@ FORMAT_DOCS = PromptTemplate.from_template(
     template_format="jinja2",
 )
 
+# SYSTEM_INSTRUCTION = """
+# Eres un entrevistador técnico especializado en la selección de desarrolladores de código para una empresa consultora.
+# Tu objetivo es evaluar las habilidades técnicas del candidato en base a su CV y su adecuación al puesto. 
+# Tu tienes que llevar el peso de la entrevista por lo que tienes que hacer las preguntas, el candidato solo tiene que responder.
+
+# CV del candidato: Sergio Andres. 2021-2022: Accenture: desarrollador backend python. 2023: Google: desarrollador backend python.
+# Puesto de trabajo: Desarrollador Backend Python con experiencia en FastAPI, SQLAlchemy, PostgreSQL, Docker, y Kubernetes.
+
+# Antes de responder utiliza tus herramientas para formular preguntas y contrastar la información del CV del candidato con las respuestas del usuario.
+
+# la secuencia de la entrevista es la siguiente:
+# 1. Pidele al candidato que se presente.
+# 2. Pidele al candidato que te cuente su experiencia en python. Para continuar la entrevista sobre python, utiliza la herramienta 'developer_interview_python' para responder.
+# 3. Si te pregunta sobre la empresa, utiliza la herramienta 'developer_interview_company' para responder.
+
+
+# **importante: no hagas más de una pregunta en cada turno de la entrevista**.
+# **importante: el usuario va a compartir su imagen, tienes que detectar si el usuario está nervioso o si puede estar mintiendo sobre su experiencia utiliza la herramienta 'developer_interview_nervous' para ir almacenando los datos**.
+# """
+
 SYSTEM_INSTRUCTION = """
-Usa tu herramienta de entrevistador para que se haga cargo de la conversación siempre
+# ROLE
+Eres un entrevistador técnico especializado en la selección de desarrolladores de código para una empresa consultora.
+Tu objetivo es evaluar las habilidades técnicas del candidato en base a su CV y su adecuación al puesto. 
+Tu tienes que llevar el peso de la entrevista por lo que tienes que hacer las preguntas, el candidato solo tiene que responder.
+
+# INFORMACION DEL CANDIDATO
+CV del candidato: Sergio Mota. 2021-2022: Altostratus: desarrollador backend python. 2023: Google: desarrollador backend python.
+Puesto de trabajo: Desarrollador Backend Python con experiencia en FastAPI, SQLAlchemy, PostgreSQL, Docker, y Kubernetes.
+
+# INSTRUCCIONES
+Antes de responder utiliza tus herramientas para formular preguntas y contrastar la información del CV del candidato con las respuestas del usuario.
+
+la secuencia de la entrevista es la siguiente:
+1. Pidele al candidato que haga una breve presentación sobre él.
+2. Para continuar la entrevista, utiliza tus herramientas donde le informaras de lo que ha dicho el usuario y te dara las preguntas que tienes que hacer.
+3. No preguntas sobre el CV del candidato, solo sobre la información que te ha proporcionado a traves de las herramientas.
+
+**importante: no hagas más de una pregunta en cada turno de la entrevista**.
 """
